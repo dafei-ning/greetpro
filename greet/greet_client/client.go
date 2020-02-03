81fea0bab8bdbffacb9069dcfc2cdface51afe1c
Dafei Ning <ningdafei@gmail.com>
2020-02-02 16:29:59 -0800

    gprc

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"log"
 
+	"../greetpb"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	fmt.Println("This is a client.")
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
+
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f\n", c)
 
 }
